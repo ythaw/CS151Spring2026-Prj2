da406523230508c81cbca158ce5749d331d58602
MinHydro <ghostcl270405@gmail.com>
2026-03-17 23:02:23 -0700

    es

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,5 +1,5 @@
 package model;
 
-public class Account {
+public abstract class Account {
     
 }
--- src/model/StudentAccount.java
+++ src/model/StudentAccount.java
@@ -1,5 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentAccount {
+    private String major;
+    private int currentCredits;
+    private List<Section> enrolledSections;
+    private List<Course> completeCourses;
+
+    public StudentAccount(String major, int currentCredits){
+      this.major = major;
+      this.currentCredits = currentCredits;
+      this.enrolledSections = new ArrayList<>();
+      this.completeCourses = new ArrayList<>();
+    }
     
+    public int CalculateCurrentCreditLoad(){
+      int total = 0;
+      for (Section s : enrolledSections){
+        total += s.getCredits();
+      }
+      return total;
+    }
 }
