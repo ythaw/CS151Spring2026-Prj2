bc7797bfefe9395e3a509f438d5fcf1b9ed3f318
Thiha Lin <thihayelin03@gmail.com>
2026-03-17 21:44:45 -0700

    fill Main and make the start of Menu

--- src/exceptions/InactiveEntityException.java
+++ src/exceptions/InactiveEntityException.java
@@ -1,4 +1,4 @@
-package main.exceptions;
+package exceptions;
 
 public class InactiveEntityException {
     
--- src/system/RegistrationSystem.java
+++ src/system/RegistrationSystem.java
@@ -1,4 +1,4 @@
-package main.system;
+package system;
 
 public class RegistrationSystem {
     
